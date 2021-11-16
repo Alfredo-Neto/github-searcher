70543c4910a921cdf82e666a694f280519129c23
Alfredo Braule <alfredoneto934@gmail.com>
2021-11-16 03:39:17 -0300

    initial

---
+++ translater.rb
@@ -0,0 +1,8 @@
+class Translater
+    puts "Insira seu texto"
+    text = gets.chomp
+    puts "Insira o idioma em que o seu texto está escrito"
+    from = gets.chomp
+    puts "Insira o idioma para o qual você quer traduzi-lo"
+    to = gets.chomp
+end
